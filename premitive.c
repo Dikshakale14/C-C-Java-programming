3cff7421cae4f714a12229ad9f97870234dd6be1
Dikshakale14 <dikshakale0800@gmail.com>
2026-06-22 11:38:29 +0530

    premitive.c

---
+++ premitive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  char ch='A';
+  int i=22;
+  float f= 90.6f;
+  double d= 78.556634;
+
+printf("R values of all the varibales\n");
+
+printf("%c\n",ch);
+printf("%d\n",i);
+printf("%f\n",f);
+printf("%lf\n",d);
+
+return 0;
+}
+
