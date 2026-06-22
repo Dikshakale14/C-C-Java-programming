b16c2ef2ebba0343568cb41f46de8cd60c9d68cf
Dikshakale14 <dikshakale0800@gmail.com>
2026-06-22 12:14:04 +0530

    sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i = 10;
+int j =20;
+
+i++; // increment
+j++; // decrement
+
+printf("value of i :%d\n",i);  //11
+printf("value of j :%d\n",j); //9
+
+return 0;
+}
