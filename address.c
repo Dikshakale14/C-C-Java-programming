42e457ff16dc3f923ef1d1b9678072064cbb0baa
Dikshakale14 <dikshakale0800@gmail.com>
2026-06-22 11:47:25 +0530

    address.c

---
+++ address.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+char ch = 'D';
+int i =33;
+float f= 10.5f;
+double d= 30.43302;
+
+printf("l values of variables are:\n");
+
+pritnf("%u\n",&ch);
+printf("%u\n",&i)
+printf("%u\n",&f);
+printf("%u\n",&d);
+
+return 0;
+}
+
