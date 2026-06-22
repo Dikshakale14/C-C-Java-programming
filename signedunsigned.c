3281e36aa56d996b52097228772b676957a6fe4e
Dikshakale14 <dikshakale0800@gmail.com>
2026-06-22 16:47:26 +0530

    Create signedunsigned.c

---
+++ signedunsigned.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 11;             // signed
+    int j = -11;            // signed  can be both pos as well as neg
+    unsigned int k = 11;    // unsigned (only be positive eg :age)
+    signed int a = 11;      // signed
+    signed int b = -11;     // signed
+    
+    return 0;
+}
